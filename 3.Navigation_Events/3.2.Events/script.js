f2b3d8e540e5ca2ab45d0428f3c62b6dd53c6042
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-05 11:01:21 +0100

    commenting js

--- 3.Navigation_Events/3.2.Events/script.js
+++ 3.Navigation_Events/3.2.Events/script.js
@@ -6,22 +6,28 @@ const getElapsedTime = () => {
 
 let displayed = document.querySelector(".displayedsquare-wrapper");
 
+// creates keyboard event
 window.addEventListener('keydown', (keyboard) => {
   console.log(keyboard.code);
 
   if (keyboard.code === "Space"){
+
+    //random numbers for colors
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
 
     var changedBG = `rgb(${r},${g},${b})`;
     document.body.style.backgroundColor = changedBG; 
+
+    // log for backgrround color
     let newLog = document.createElement("li");
     let logText = document.createTextNode(`[${getElapsedTime()}] Changed background color to ${changedBG}`);
     newLog.appendChild(logText);
     document.querySelector("ul").appendChild(newLog);
   }
 
+  // deletes log
   if (keyboard.code === "KeyL")
   {
    let logContent = document.querySelector("ul").children;
@@ -31,6 +37,7 @@ window.addEventListener('keydown', (keyboard) => {
    }
   }
 
+  //deletes generated squares
   if (keyboard.code === "KeyS")
   {
    let alldisplayed = document.querySelector(".displayedsquare-wrapper").children;
@@ -41,6 +48,7 @@ window.addEventListener('keydown', (keyboard) => {
   }
 })
 
+// creates alert for the clicked squares
 function showMeYourColor(square){
   alert(square[1]);
 }
@@ -49,10 +57,15 @@ const clickOnSquare = (e) => {
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
 
+
+  // creates square
   let newDiv = document.createElement("div");
   let newLog = document.createElement("li");
+
   newDiv.classList.add("displayedsquare");
   let colorOfSquare = "";
+
+  // chooses the color class to give and prepares for the log list
   if (e.target.classList.contains("green")){
     newDiv.classList.add("green");
     colorOfSquare = "green";
@@ -63,8 +76,11 @@ const clickOnSquare = (e) => {
     newDiv.classList.add("orange");
     colorOfSquare = "orange";
   } 
+
+  // logs color
   let logText = document.createTextNode(`[${getElapsedTime()}] Created a new ${colorOfSquare} square`);
 
+  // appends created square to the container
   newLog.appendChild(logText);
   document.querySelector("ul").appendChild(newLog);
   newDiv.setAttribute('onclick', "showMeYourColor(this.classList);");
@@ -75,9 +91,3 @@ const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
-
-displayed.chilren.addEventListener('click', (e) => {
-  alert(displayed[e].target.classList[1])}
-)
-
-
